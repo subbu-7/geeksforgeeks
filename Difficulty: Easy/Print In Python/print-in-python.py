5cccaa97a0f4b255f53d7c7226610f506ac15479
SUBRAHMANYAM ANADASU <subramanyam.anadasu@gmail.com>
2026-02-13 23:55:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print In Python/print-in-python.py
@@ -0,0 +1,15 @@
+a = input()
+b = input()
+separator = input()[0]
+
+# Print with space
+print(a, b)
+
+# Print without newline at the end
+print(a, b, end="")
+
+# Print with separator
+print(a, b, sep=separator)
+
+# Print without space
+print(a + b)
