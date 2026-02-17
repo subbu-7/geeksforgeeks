fbd9a35e9c1c8bdc8e167db28832f8f1961b0cb6
SUBRAHMANYAM ANADASU <subramanyam.anadasu@gmail.com>
2026-02-17 23:22:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap The Numbers/swap-the-numbers.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+
+########### Write your code below ###############
+
+# Write Code to Swap
+temp = a
+a = b
+b = temp
+
+
+########### Write your code above ###############
+
+print(a, b)
