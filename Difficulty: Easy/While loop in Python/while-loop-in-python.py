5f2b2472c6b3e96517196b70b834526f112acf97
SUBRAHMANYAM ANADASU <subramanyam.anadasu@gmail.com>
2026-02-18 22:02:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/While loop in Python/while-loop-in-python.py
@@ -0,0 +1,10 @@
+def printInDecreasing(x):
+    # code here
+    while (x >= 0):
+
+        # your statement below to print the number
+        # in decreasing order
+        # Note: use end=" " parameter with print to seperate numbers by space.
+        ##Output for testcases will automatically separated by a new line by the print() in driver code
+        print(x,end = " ")
+        x -= 1
